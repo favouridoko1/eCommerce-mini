055955cbb6cd0f783e8d7e881929f8daf25f957b
Favour Idoko <ojodomofavour2004@gmail.com>
2025-06-19 23:45:38 +0100

    Initial Commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='text-[#1F2937] bg-[#F3F4F6] h-[228px]'>Footer</footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import { logo } from "@/app/assets";
+import { LuSearch } from "react-icons/lu";
+import Image from "next/image";
+import Link from "next/link";
+import { PiBag } from "react-icons/pi";
+
+const navItems = [
+  { name: "HOME", href: "/" },
+  { name: "ABOUT US", href: "/about-us" },
+  { name: "SHOP", href: "/products" },
+  { name: "CONTACT", href: "/contact" },
+];
+const Header = () => {
+  return (
+    <header className="h-[69px] bg-white text-[#1F2937] flex items-center justify-between md:px-[84px]">
+      <figure>
+        <Image src={logo} alt={"Bran Logo"}  />
+      </figure>
+      <nav className="flex gap-[10px]">
+        {navItems.map((el, index) => (
+          <Link key={index} href={el.href} className="text-xs leading-[135%] font-semibold">
+           {el.name}
+          </Link>
+        ))}
+      </nav>
+      <div className="flex gap-[10px]">
+        <PiBag size={18} />
+        <LuSearch size={18} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
